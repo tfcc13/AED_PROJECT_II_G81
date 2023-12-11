814ff089e6ce2e51a5dda5ca1ffa7dd49826e676
Tiago <tiagofccarvalho13@gmail.com>
2023-12-11 17:56:01 +0000

    Updated airline class

--- src/Airline.cpp
+++ src/Airline.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "Airline.h"
+
+Airline::Airline(const string& airline_code, const string& airline_name, const string& callsign, const string& country) {
+
+    airline_code_ = airline_code;
+    airline_name_ = airline_name;
+    callsign_ = callsign;
+    country_ = country;
+
+}
+
+
+
+const string &Airline::getAirlineCode() const {
+    return airline_code_;
+}
+
+const string &Airline::getAirlineName() const {
+    return airline_name_;
+}
+
+
+const string &Airline::getAirlineCallsign() const {
+    return callsign_;
+}
+
+const string &Airline::getAirlineCountry() const {
+    return country_;
+}
--- src/Airline.h
+++ src/Airline.h
@@ -4,9 +4,49 @@
 
 #ifndef PROJECT_II_AIRLINE_H
 #define PROJECT_II_AIRLINE_H
+#include <string>
 
+using namespace std;
+/// Class that represents an Airline Company
 
 class Airline {
+
+private:
+    string airline_code_;
+    string airline_name_;
+    string callsign_;
+    string country_;
+
+public:
+
+    ///Airline Constructor
+    ///
+    /// \param airline_code Airline unique code
+    /// \param airline_name Airline name
+    /// \param callsign Airline callsign
+    /// \param country Airline headquarters country
+
+    Airline(const string & airline_code, const string & airline_name, const string & callsign, const string & country );
+
+
+    /// Getter function of airline_code_
+    /// \return airline_code_
+    const string& getAirlineCode() const;
+
+    /// Getter function of airline_name_
+    /// \return airline_name_
+    const string& getAirlineName() const;
+
+    /// Getter function of callsign_
+    /// \return callsign_
+    const string& getAirlineCallsign() const;
+
+
+    /// Getter function of country_
+    /// \return country_
+    const string& getAirlineCountry() const;
+
+
 
 
 

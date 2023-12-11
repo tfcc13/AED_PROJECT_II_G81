bc04a19cff2842a0730ba044ec81bfb52acd11c7
Tiago <tiagofccarvalho13@gmail.com>
2023-12-11 16:31:12 +0000

    Added cmaketext and add Airlines

---
+++ Airline.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tiago on 11-12-2023.
+//
+
+#include "Airline.h"
---
+++ Airline.h
@@ -0,0 +1,14 @@
+//
+// Created by tiago on 11-12-2023.
+//
+
+#ifndef PROJECT_II_AIRLINE_H
+#define PROJECT_II_AIRLINE_H
+
+
+class Airline {
+
+};
+
+
+#endif //PROJECT_II_AIRLINE_H
